eb375ca72827939c92d9f6acb081fec0269a7ec5
Aigul <sharipova.aig@gmail.com>
2022-06-24 12:27:47 -0400

    forEach

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const movie = document.querySelectorAll('movie');
 inputField.addEventListener('keyup', function(event) {
   const word = event.target.value.toLowerCase;
 })
+
+divs.forEach(item => { item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block')
+  
+});
