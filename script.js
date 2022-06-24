ea3ddd37c8b593e74e2f7920aea1571a4a4b7e63
Aigul <sharipova.aig@gmail.com>
2022-06-24 12:24:00 -0400

    lower case

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const inputField = document.querySelector('.input-field');
 const movie = document.querySelectorAll('movie');
 
 inputField.addEventListener('keyup', function(event) {
-  const word = event.target.value;
+  const word = event.target.value.toLowerCase;
 })
