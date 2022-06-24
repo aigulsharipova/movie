6a4093a4e0880f8fe74773b4847f78df0e472515
Aigul <sharipova.aig@gmail.com>
2022-06-24 12:37:47 -0400

    eventListener func

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const movie = document.querySelectorAll('movie');
 
 inputField.addEventListener('keyup', function(event) {
   const word = event.target.value.toLowerCase();
-})
 
-movie.forEach(item => { item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none');
-})
+
+movie.forEach(item => {item.querySelector('p').textContent.toLowerCase().includes(word) ? (item.style.display = 'block') : (item.style.display = 'none');
+}) })
